db319e715ff6647650cc04fe9d5a3054b539f503
Anil Kodamunja <anilkodamunja864@gmail.com>
2025-05-28 09:09:55 +0530

    Fix for netlify 404 Error

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  base: './', // keeps assets relative for Netlify
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: 'src/App.jsx', // Specify App.jsx as the entry point
-    },
-  },
 });
